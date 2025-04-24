49608095bb0b280400116b9bd716da0db7ab9d9a
ARAVIND . S <78127293+aravind51097@users.noreply.github.com>
2025-04-24 21:13:54 +0530

    Array

---
+++ TUF_DSA/src/com/java/Array/LinearSearch.java
@@ -0,0 +1,22 @@
+package com.java.Array;
+
+public class LinearSearch {
+	public static void main(String[] args) {
+		
+		int[] a = {1,2,3,4,5,3,3,2};
+		int target = 3;
+		System.out.println(linearSearching(a, target));
+		
+	}
+	
+	public static int linearSearching(int[] arr , int target ) 
+	{
+		for(int i=0 ; i<arr.length ; i++) {
+			if(arr[i] == target) {
+				return 1;
+			}
+		}		
+		return -1;
+	}
+
+}
